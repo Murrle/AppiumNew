0eab9c1304c7c9c2d63710d17dbcc77b6b4515a5
Admin <Admin@192.168.1.118>
2019-05-22 10:14:33 +0530

    test

---
+++ PhpTravels/Feature/myfeature.feature
@@ -0,0 +1,11 @@
+
+@tag
+Feature: verify PHP login
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: login page
+    Given user has to login
+    
+
+ 
---
+++ PhpTravels/src/StepDefinition/Stepdefinition.java
@@ -0,0 +1,46 @@
+package StepDefinition;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+
+import cucumber.api.java.en.Given;
+
+public class Stepdefinition {
+	@Given("user has to login")
+	public void openbrowser() throws IOException{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.phptravels.net/login");
+		//driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("user@phptravels.com");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demouser");
+		driver.findElement(By.xpath("//button[@type='submit']")).sendKeys(Keys.ENTER);
+	String test =	driver.findElement(By.xpath("//*[@class='RTL']")).getText();
+	System.out.println(test);
+	File scrfile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(scrfile, new File("D:/test.jpg"));
+	if(test.contains("Johny")){
+		
+		Reporter.log("Passed");
+	}
+		driver.quit();
+		
+		
+		
+		
+		
+	}
+	
+}
